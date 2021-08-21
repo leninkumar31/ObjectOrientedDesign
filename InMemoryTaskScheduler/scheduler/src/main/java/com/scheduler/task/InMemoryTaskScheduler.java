aef6d9a856c74c5d3a8e1c1f470eac424225e458
Lenin Kumar Pothabattula <lpothabattula@fanatics.com>
2021-08-21 14:36:40 +0530

    Updated with fine grained locking using lock and condition

--- InMemoryTaskScheduler/scheduler/src/main/java/com/scheduler/task/InMemoryTaskScheduler.java
+++ InMemoryTaskScheduler/scheduler/src/main/java/com/scheduler/task/InMemoryTaskScheduler.java
@@ -4,6 +4,9 @@ import java.util.PriorityQueue;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.ReentrantLock;
 
 import com.scheduler.exceptions.TaskRejectedException;
 import com.scheduler.models.ScheduledTask;
@@ -13,17 +16,23 @@ import lombok.Getter;
 @Getter
 public class InMemoryTaskScheduler implements TaskScheduler {
 
-	private volatile long taskId;
+	private volatile AtomicLong taskId;
 
 	private ExecutorService threadPoolExecutor;
 
 	private PriorityQueue<ScheduledTask> delayQueue;
-	
+
 	private Thread scheduler;
+
+	private ReentrantLock lock;
+
+	private Condition newTaskArrived;
 
 	public InMemoryTaskScheduler(int workerThreads) {
 		this.threadPoolExecutor = Executors.newFixedThreadPool(workerThreads);
-		this.taskId = 0;
+		this.taskId = new AtomicLong();
+		this.lock = new ReentrantLock();
+		this.newTaskArrived = this.lock.newCondition();
 		this.delayQueue = new PriorityQueue<>();
 		scheduler = new Thread(() -> {
 			run();
@@ -32,26 +41,28 @@ public class InMemoryTaskScheduler implements TaskScheduler {
 	}
 
 	@Override
-	public synchronized long schedule(Runnable task, long delay, TimeUnit unit) throws TaskRejectedException {
-		this.taskId++;
-		ScheduledTask scheduledTask = new ScheduledTask(this.taskId, task, delay, unit);
-		System.out.println(scheduledTask.getId());
+	public long schedule(Runnable task, long delay, TimeUnit unit) throws TaskRejectedException {
+		this.taskId.incrementAndGet();
+		ScheduledTask scheduledTask = new ScheduledTask(this.taskId.get(), task, delay, unit);
+		lock.lock();
 		delayQueue.add(scheduledTask);
-		notify();
+		this.newTaskArrived.signal();
+		lock.unlock();
 		return scheduledTask.getId();
 	}
 
 	@Override
-	public synchronized long scheduleAtFixedInterval(Runnable task, long initDelay, long interval, TimeUnit unit)
+	public long scheduleAtFixedInterval(Runnable task, long initDelay, long interval, TimeUnit unit)
 			throws TaskRejectedException {
-		this.taskId++;
-		ScheduledTask scheduledTask = new ScheduledTask(this.taskId, task, initDelay, interval, unit);
-		System.out.println(scheduledTask.getId());
+		this.taskId.incrementAndGet();
+		ScheduledTask scheduledTask = new ScheduledTask(this.taskId.get(), task, initDelay, interval, unit);
+		lock.lock();
 		delayQueue.add(scheduledTask);
-		notify();
+		this.newTaskArrived.signal();
+		lock.unlock();
 		return scheduledTask.getId();
 	}
-	
+
 	@Override
 	public void start() {
 		this.scheduler.start();
@@ -59,37 +70,36 @@ public class InMemoryTaskScheduler implements TaskScheduler {
 
 	private void run() {
 		for (;;) {
-			synchronized (this) {
-				while (delayQueue.isEmpty()) {
-					try {
-						wait();
-					} catch (InterruptedException e) {
-						System.out.println("Interrupted");
-					}
+			lock.lock();
+			while (delayQueue.isEmpty()) {
+				try {
+					this.newTaskArrived.await();
+				} catch (InterruptedException e) {
+					System.out.println("Interrupted");
 				}
-				while (!delayQueue.isEmpty()) {
-					ScheduledTask task = delayQueue.peek();
-					long timeDiff = task.getNextExecutionTimeInMs() - System.currentTimeMillis();
-					if (timeDiff <= 0) {
-						System.out.println("task :" + task.getId() + "is supposed to execute at:"
-								+ task.getNextExecutionTimeInMs());
-						threadPoolExecutor.execute(task.getTask());
-						ScheduledTask removedTask = delayQueue.poll();
-						if (removedTask.isPeriodic()) {
-							removedTask.setNextExecutionTimeInMs(removedTask.getNextExecutionTimeInMs()
-									+ task.getUnit().toMillis(removedTask.getInterval()));
-							delayQueue.add(removedTask);
-						}
-						notifyAll();
-					} else {
-						try {
-							wait(timeDiff);
-						} catch (InterruptedException e) {
-							System.out.println("interrupted");
-						}
+			}
+			while (!delayQueue.isEmpty()) {
+				ScheduledTask task = delayQueue.peek();
+				long timeDiff = task.getNextExecutionTimeInMs() - System.currentTimeMillis();
+				if (timeDiff <= 0) {
+					System.out.println(
+							"task :" + task.getId() + "is supposed to execute at:" + task.getNextExecutionTimeInMs());
+					threadPoolExecutor.execute(task.getTask());
+					ScheduledTask removedTask = delayQueue.poll();
+					if (removedTask.isPeriodic()) {
+						removedTask.setNextExecutionTimeInMs(removedTask.getNextExecutionTimeInMs()
+								+ task.getUnit().toMillis(removedTask.getInterval()));
+						delayQueue.add(removedTask);
+					}
+				} else {
+					try {
+						this.newTaskArrived.await(timeDiff, TimeUnit.MILLISECONDS);
+					} catch (InterruptedException e) {
+						System.out.println("interrupted");
 					}
 				}
 			}
+			lock.unlock();
 		}
 	}
 
